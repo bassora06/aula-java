7a619c1ea406c77f34b71f1941c22aecee989c14
Lucas Bassora da Silva <lucasbassora07@gmail.com>
2024-09-19 22:22:06 -0300

    classes de java

--- JavaAplication3/src/javaaplication3/JavaAplication3.java
+++ JavaAplication3/src/javaaplication3/JavaAplication3.java
@@ -6,19 +6,14 @@ public class JavaAplication3 {
 
    
     public static void main(String[] args) {
-       
-        /*Veiculo v = new Veiculo(4, "V8");
-        
-        System.out.println("Rodas: "+ v.getRodas());
-        System.out.println("Motor: " +v.getMotor());
-        
+            
         Carro c = new Carro();
         c.imprimir();
         System.out.println("Qtde:"+c.quantidade(3)+"\n");
         
         Moto m = new Moto();
         m.imprimir();
-        System.out.println("Qtde da moto:"+m.quantidade(10));*/
+        System.out.println("Qtde da moto:"+m.quantidade(10));
         
     }
     
--- JavaAplication3/src/javaaplication3/Moto.java
+++ JavaAplication3/src/javaaplication3/Moto.java
@@ -2,7 +2,27 @@
 package javaaplication3;
 
 
-public class Moto{
+public class Moto extends Terrestres{
+
+    @Override
+    public void mover() {
+        System.out.println("Andando");
+    }
+
+    @Override
+    public void abastecer() {
+        System.out.println("Gasolina/Alcool");
+    }
+
+    @Override
+    public void imprimir() {
+        System.out.println("Harley davidson 1500cc");
+    }
+
+    @Override
+    public int quantidade(int qtde) {
+        return qtde;
+    }
 
     
     
